01fc7b487638eb99ba7144ed02731db107d41f04
Ewerthon Izidoro <72260672+Ewerthon-izi@users.noreply.github.com>
2023-01-23 01:59:19 -0300

    add abstract class, begin of board

---
+++ xadrezCsharp/xadrezCsharp/partida/PartidaXadrez.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using xadrezCsharp.tabuleiro;
+
+namespace xadrezCsharp.partida
+{
+    internal class PartidaXadrez
+    {
+        public Tabuleiro tab { get; private set; }
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
+        public bool terminada { get; set; }
+        private HashSet<Peca> pecas;
+        private HashSet<Peca> capturadas;
+        public Peca vulneravelEnPassant { get; private set; }
+        public bool xeque { get; private set; }
+
+        public PartidaXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branco;
+            xeque = false;
+            vulneravelEnPassant = null;
+            terminada = false;
+            pecas = new HashSet<Peca>();
+            capturadas = new HashSet<Peca>();
+            colocarPecas();
+        }
+
+        public Peca executarMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarMovimentos();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(destino, p);
+            //Verifica se uma peca foi comida
+            if (pecaCapturada != null)
+            {
+                capturadas.Add(pecaCapturada);
+            }
+        }
+    }
+}
---
+++ xadrezCsharp/xadrezCsharp/tabuleiro/tabuleiro.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xadrezCsharp.tabuleiro;
+
+
+namespace xadrezCsharp.tabuleiro
+{
+    public class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Posicao pos, Peca p)
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Ja existe uma pessa nessa posição!");
+            }
+            pecas[pos.Linha, pos.Coluna] = p;
+            p.posicao = pos;
+        }
+
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.Linha, pos.Coluna] = null;
+            return aux;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Invalida!");
+            }
+        }
+    }
+}
