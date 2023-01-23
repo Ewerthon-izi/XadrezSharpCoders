5dd6eb8598d7e8afa144bb5ab61cfe2c30fb2f02
Ewerthon Izidoro <72260672+Ewerthon-izi@users.noreply.github.com>
2023-01-23 19:26:24 -0300

    add exception in the main

--- xadrezCsharp/xadrezCsharp/Program.cs
+++ xadrezCsharp/xadrezCsharp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using xadrezCsharp.partida;
+using xadrezCsharp.tabuleiro;
+
+static void Main(string[] args)
+{
+    try
+    {
+        PartidaXadrez partida = new PartidaXadrez();
+        Console.WriteLine(partida.turno);
+    }
+    catch (TabuleiroException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
