3b5ed7b4f4aba3d5d2704d253fc4c7c945e8e7d9
Ewerthon-izi <ewer.izi@gmail.com>
2023-01-24 18:06:32 -0300

    add horse and chess rules

--- xadrezCsharp/xadrezCsharp/Program.cs
+++ xadrezCsharp/xadrezCsharp/Program.cs
@@ -1,3 +1,4 @@
+using xadrezCsharp;
 using xadrezCsharp.partida;
 using xadrezCsharp.tabuleiro;
 
@@ -6,7 +7,43 @@ static void Main(string[] args)
     try
     {
         PartidaXadrez partida = new PartidaXadrez();
-        Console.WriteLine(partida.turno);
+        while (!partida.terminada)
+        {
+            try
+            {
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+
+                Console.WriteLine();
+                Console.Write("Origem: ");
+                Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                partida.validarPosicaoDeOrigem(origem);
+
+                bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                Console.Clear();
+                Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                Console.WriteLine();
+                Console.Write("Destino: ");
+                Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                partida.validarPosicaoDeDestino(origem, destino);
+
+                partida.realizarJogada(origem, destino);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+        Console.Clear();
+        Tela.imprimirPartida(partida);
     }
     catch (TabuleiroException e)
     {
@@ -18,3 +55,4 @@ static void Main(string[] args)
         Console.ReadLine();
     }
 }
+
